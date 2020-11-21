19e5e9f4ed6cd798c7c806031f56e4c9966b1d23
jawa <jawaharsenthil1411@gmail.com>
2020-11-21 17:13:48 +0530

    first commit

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 
 def translate(word):
     # driver = webdriver.Firefox()
-    firefox_binary = FirefoxBinary('/usr/bin/firefox/')
+    firefox_binary = FirefoxBinary('/usr/bin/firefox')
     driver = webdriver.Firefox(firefox_binary=firefox_binary)
 
     driver.get("http://dictionary.tamilcube.com")

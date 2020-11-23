cecd2824fce18e3f6a7875dbe9d3272475fffbf3
jawa <jawaharsenthil1411@gmail.com>
2020-11-23 16:22:39 +0530

    first commit

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import re
 from credentials import bot_token, bot_user_name,URL
 # from selenium.webdriver.chrome.options import Options
 
-GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
+GOOGLE_CHROME_BIN = '/app/.apt/usr/bin/google_chrome'
 CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
 global bot
 global TOKEN
@@ -19,7 +19,7 @@ def translate(word):
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--no-sandbox')
-    chrome_options.binary_location = GOOGLE_CHROME_PATH
+    chrome_options.binary_location = GOOGLE_CHROME_BIN
 
     driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
     

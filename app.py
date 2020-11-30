32f2c42ec8a055d82d5f49987047259f3e67e240
jawa <jawaharsenthil1411@gmail.com>
2020-11-30 17:53:25 +0530

    first commit

--- app.py
+++ app.py
@@ -20,9 +20,9 @@ def translate(word):
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--no-sandbox')
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.binary_location = "GOOGLE_CHROME_BIN"
 
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    driver = webdriver.Chrome(executable_path="CHROMEDRIVER_PATH", chrome_options=chrome_options)
     
     driver.get("http://dictionary.tamilcube.com")
 
